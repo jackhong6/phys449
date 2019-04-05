a55720b71cb16dc7107bc1b5ac2706c3cfade4d8
Jack Hong <jackhong6@gmail.com>
2019-04-04 19:27:43 -0700

    Deleted tmp.m

--- tmp.m
+++
@@ -1,5 +0,0 @@
-[lat, long] = meshgrid(linspace(-90, 90, 20), linspace(-180, 180, 40));
-Z = randn(size(lat));
-axesm eckert4;
-framem; gridm;
-h = geoshow(lat, long, Z, 'DisplayType', 'texturemap');
